7e981a77250e716a72b75b37425f4719e01aeb86
Rafael Lotar Wruck <rafawruck@gmail.com>
2025-01-14 22:05:36 -0300

    Add files via upload

--- SegundaVersão.py
+++ SegundaVersão.py
@@ -13,9 +13,33 @@ Dados = {"Data": [],
          "Apiuna":[],
          "Timbó":[]
          }
-
+dataMes = {"DiaInicial": 0,"MesInicial": 0}
 Evento = False
 
+#fazer função correta para data e mês
+def verificaEvento():
+    global Dados
+    if len(Dados["Data"]) == 0:
+        print ("Não existe dados sobre eventos.")
+        print ("Vamos iniciar o cadastro do evento.")
+        dataMes["DiaInicial"] = int(input("Insira o dia de inicio: "))
+        dataMes["MesInicial"] = int(input("Insira o Mês de inicio: "))
+        print ("Data de inicio do evento será {}/{} e horário as 00:00 horas".format( dataMes["DiaInicial"], dataMes["MesInicial"]))
+    else:
+        print ("Existe um evento em regitro.")
+        print ("Ultimo dado registrado foi da data {} das {} horas".format(Dados["Data"][(len(Dados["Data"]-1))],Dados["Hora"][len(Dados["Hora"]-1)]))
+        
+        
+        
+        
+            
+
+
+
+
+
+
+    
 
 def registraLeitura():
     global Dados
@@ -25,11 +49,11 @@ def registraLeitura():
         if start == "S":
             diaI = int(input("Insira o dia de inicio: "))
             mesI = int(input("Insira o mês de inicio: "))
-            dataI = ("{}/{}".format(diaI,mesI))
-            print ("Data de inicio do evento será {} e horário as 00:00 horas".format(dataI))
+            print ("Data de inicio do evento será {}/{} e horário as 00:00 horas".format(diaI,mesI))
             
     while True:
         posição = len(Dados["Data"])
+        dataI = ("{}/{}".format(diaI,mesI))
         Dados["Data"].append(dataI)
         Dados["Hora"].append(horas[posição])
         Dados["Blumenau"].append(float(input("Insira a leitura de Blumenau para o dia {} as {}: ".format(Dados["Data"][posição], Dados["Hora"][posição]))))
